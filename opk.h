3546c71eab76194c6948913679a64f7fd2f1dbaf
Paul Cercueil <paul@crapouillou.net>
2012-10-06 19:50:36 +0200

    Make the opk.h file usable from C++ programs.

--- opk.h
+++ opk.h
@@ -2,11 +2,19 @@
 #ifndef OPK_H
 #define OPK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct ParserData;
 
 struct ParserData *openMetadata(const char *opk_filename);
 void closeMetadata(struct ParserData *pdata);
 
 char *readParam(struct ParserData *pdata, const char *name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* OPK_H */
