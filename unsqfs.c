pcercuei/libopk

commit af564f19d7645b06b4162325c6ee08a712a55230

unsqfs: Reduce the use of memcpy
For uncompressed blocks, one memcpy is saved, for compressed blocks
two. Also, write_buf() now only allocates a buffer if it has to load
a fragment, while it always allocated a buffer before.

Also added some sanity checks on compressed and uncompressed block
sizes. Only broken images would trigger these.

unsqfs.c
@@ -637,30 +637,41 @@ static struct cache *cache_init()
 	return cache;
 }
 
-static bool read_data_block(struct PkgData *pdata, void *data,
+static bool read_data_block(struct PkgData *pdata, void *buf, int buf_size,
 		long long block, int size)
 {
-	if (!read_fs_bytes(pdata->fd, block,
-			SQUASHFS_COMPRESSED_SIZE_BLOCK(size), data)) {
-		return false;
-	}
+	const int csize = SQUASHFS_COMPRESSED_SIZE_BLOCK(size);
+	if (SQUASHFS_COMPRESSED_BLOCK(size)) { // compressed block
+		if (csize >= buf_size) {
+			// In the case compression doesn't make a block smaller,
+			// mksquashfs will store the block uncompressed.
+			ERROR("Refusing to load too-large compressed block\n");
+			return false;
+		}
 
-	if (SQUASHFS_COMPRESSED_BLOCK(size)) {
-		char tmp[pdata->sBlk.block_size];
-		int error, res;
+		// Load compressed data into temporary buffer.
+		char tmp[csize];
+		if (!read_fs_bytes(pdata->fd, block, sizeof(tmp), tmp)) {
+			return false;
+		}
 
-		res = squashfs_uncompress(pdata, tmp, data,
-				SQUASHFS_COMPRESSED_SIZE_BLOCK(size),
-				pdata->sBlk.block_size, &error);
-
+		int error, res = squashfs_uncompress(
+				pdata, buf, tmp, sizeof(tmp), buf_size, &error);
 		if (res == -1) {
 			ERROR("Uncompress failed with error code %d\n", error);
 			return false;
 		}
-		memcpy(data, tmp, res);
-	}
 
-	return true;
+		return true;
+
+	} else { // uncompressed block
+		if (csize != buf_size) {
+			ERROR("Refusing to load size-mismatched uncompressed block\n");
+			return false;
+		}
+
+		return read_fs_bytes(pdata->fd, block, buf_size, buf);
+	}
 }
 
 static bool add_entry(struct hash_table_entry *hash_table[], long long start,
@@ -813,12 +824,6 @@ static bool write_buf(struct PkgData *pdata, struct inode *inode, char *buf)
 {
 	TRACE("write_buf: regular file, blocks %d\n", inode->blocks);
 
-	void *data = malloc(pdata->sBlk.block_size);
-	if (!data) {
-		ERROR("Failed to allocate block data buffer\n");
-		return false;
-	}
-
 	const int file_end = inode->data / pdata->sBlk.block_size;
 	long long start = inode->start;
 	for (int i = 0; i < inode->blocks; i++) {
@@ -828,15 +833,13 @@ static bool write_buf(struct PkgData *pdata, struct inode *inode, char *buf)
 			? inode->data & (pdata->sBlk.block_size - 1)
 			: pdata->sBlk.block_size;
 
-		if (csize == 0) { /* sparse file */
+		if (csize == 0) { // sparse file
 			memset(buf, 0, size);
 		} else {
-			if (!read_data_block(pdata, data, start, csize)) {
-				goto fail_free_data;
+			if (!read_data_block(pdata, buf, size, start, csize)) {
+				return false;
 			}
 			start += SQUASHFS_COMPRESSED_SIZE_BLOCK(csize);
-
-			memcpy(buf, data, size);
 		}
 		buf += size;
 	}
@@ -847,20 +850,23 @@ static bool write_buf(struct PkgData *pdata, struct inode *inode, char *buf)
 		struct squashfs_fragment_entry *fragment_entry =
 				&pdata->fragment_table[inode->fragment];
 
-		if (!read_data_block(pdata, data,
+		void *data = malloc(pdata->sBlk.block_size);
+		if (!data) {
+			ERROR("Failed to allocate block data buffer\n");
+			return false;
+		}
+
+		if (!read_data_block(pdata, data, pdata->sBlk.block_size,
 				fragment_entry->start_block, fragment_entry->size)) {
-			goto fail_free_data;
+			free(data);
+			return false;
 		}
 
 		memcpy(buf, data + inode->offset, inode->frag_bytes);
+		free(data);
 	}
 
-	free(data);
 	return true;
-
-fail_free_data:
-	free(data);
-	return false;
 }
 
 static bool uncompress_directory_table(struct PkgData *pdata)

commit f78f2c2840e6716268f020438c1beac3f86737fa

unsqfs: Fixed too-strict block size check
Fragment blocks can be smaller than the standard block size and if
the fragment block is compressed there is no way we can know how
large it will be uncompressed before we actually uncompress it.

Bug was introduced in commit af564f19.

unsqfs.c
@@ -460,8 +460,8 @@ static int read_compressed(struct PkgData *pdata,
 static int read_uncompressed(struct PkgData *pdata,
 		long long offset, int csize, void *buf, int buf_size)
 {
-	if (csize != buf_size) {
-		ERROR("Refusing to load size-mismatched uncompressed block\n");
+	if (csize > buf_size) {
+		ERROR("Refusing to load oversized uncompressed block\n");
 		return -1;
 	}
 
@@ -583,14 +583,13 @@ failed:
 	return 0;
 }
 
-static bool read_data_block(struct PkgData *pdata, void *buf, int buf_size,
+static int read_data_block(struct PkgData *pdata, void *buf, int buf_size,
 		long long offset, int c_byte)
 {
 	const int csize = SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);
-	return (SQUASHFS_COMPRESSED_BLOCK(c_byte)
+	return SQUASHFS_COMPRESSED_BLOCK(c_byte)
 		? read_compressed(pdata, offset, csize, buf, buf_size)
-		: read_uncompressed(pdata, offset, csize, buf, buf_size)
-		) != -1;
+		: read_uncompressed(pdata, offset, csize, buf, buf_size);
 }
 
 static bool write_buf(struct PkgData *pdata, struct inode *inode, void *buf)
@@ -609,7 +608,12 @@ static bool write_buf(struct PkgData *pdata, struct inode *inode, void *buf)
 		if (c_byte == 0) { // sparse file
 			memset(buf, 0, size);
 		} else {
-			if (!read_data_block(pdata, buf, size, start, c_byte)) {
+			const int usize = read_data_block(pdata, buf, size, start, c_byte);
+			if (usize < 0) {
+				return false;
+			} else if (usize != size) {
+				ERROR("Error: data block contains %d bytes, expected %d\n",
+						usize, size);
 				return false;
 			}
 			start += SQUASHFS_COMPRESSED_SIZE_BLOCK(c_byte);
@@ -629,8 +633,9 @@ static bool write_buf(struct PkgData *pdata, struct inode *inode, void *buf)
 			return false;
 		}
 
-		if (!read_data_block(pdata, data, pdata->sBlk.block_size,
-				fragment_entry->start_block, fragment_entry->size)) {
+		const int usize = read_data_block(pdata, data, pdata->sBlk.block_size,
+				fragment_entry->start_block, fragment_entry->size);
+		if (usize < 0) {
 			free(data);
 			return false;
 		}