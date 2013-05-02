9085f637db8718fbaee32c6e46f5c517327e8a3c
Paul Cercueil <paul@crapouillou.net>
2013-05-02 00:24:38 -0400

    Add Python bindings

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='libopk',
+      description='Library to extract meta-data from OPK packages',
+      url='http://github.com/gcwnow/libopk',
+      py_modules=['opk'],
+     )
